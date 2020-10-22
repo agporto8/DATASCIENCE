b8b8bd66854a29aa047b92d289a332eb421df048
Agostina Porto <agostina.porto@jalisco.gob.mx>
2020-10-22 00:04:22 +0000

    hola_mundo

--- hola_mundo.R
+++ hola_mundo.R
@@ -1,3 +1,57 @@
 print("Hola Mundo")
 
 ##git clone https://github.com/agporto8/DATASCIENCE.git
+##cd DATASCIENCE, ~/DATASCIENCE$ git status, git add ., git commit -m "Subiendo hola_mundo.R"
+
+
+my.summary <- function(x) {
+  suma <- sum(x)
+  tamaño <- length(x)
+  promedio <- suma/tamaño
+  return(promedio)
+}
+
+a <- 1:10
+z <- my.summary(a)
+
+var1 <- 3
+var2 <- 6
+
+if (var1==var2) {
+  print('Son iguales')
+} else if (var1<var2){
+  print('Es menor')
+} else {
+  print('Es mayor')
+}
+
+caso <- 'T4'
+switch (caso, 
+        T1= {x <- "Entro en el caso T1"},
+        T2= {x <- "Entro en el caso T2"},
+        T3= {x <- "Entro en el caso T3"},
+        stop("ninguno")
+        )
+v <- LETTERS[1:4]
+
+for ( i in v) {
+  print(i)
+}
+
+
+mensaje <- "dentro del while loop"
+contador <- 1
+
+while (contador < 10) {
+  print(mensaje)
+  contatdor <- contador +1
+}
+  if contador (>5) {
+    break
+  }
+
+
+
+
+
+
